66637644ee1ca92ed5f2d07d75ab93b93eeeba7f
apo-eco-rails-dev <apo.eco_rails_dev@yahoo.com>
2013-07-19 00:07:30 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fk::Application.config.secret_token = '38b62b82d3d0f4c229059abdc60d8438c1bc0f24d160bcf4f3c54d3653004332d20b49f4061349019c96d386138d7b990a030b32935c1424d69dd50ea6b97db9'
